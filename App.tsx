1d0577716a304f7d2e727a46e4ddde5e2dfe914d
YuSzmiya <skyebell13reipgmail.com>
2022-07-14 00:14:49 +0900

    helloworld!

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
-import React, {useEffect, useMemo} from "react";
-
-const App:React.FC = () => {
-    return(
-        <div className="App">
-            <h1>aaaaa</h1>
-            <p>Hello world!</p>
-        </div>
-    )
+export default function App() {
+  return (
+    <div className="App">
+      <h1>aaaaa</h1>
+      <p>Hello world!</p>
+    </div>
+  );
 }
-
-export default App
