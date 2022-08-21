a1043c80e4825d31ff0c72b2f9e172aa1cba0511
Meri11Ra1 <R.t.00293794@gmail.com>
2022-08-22 05:32:43 +0900

    uiiii

--- src/Screen/Home.tsx
+++ src/Screen/Home.tsx
@@ -1,114 +1,46 @@
-import React, { useState } from "react";
-import { Animated, View, StyleSheet, TouchableOpacity, } from "react-native";
+import react, { useRef } from 'react';
+import { 
+  Animated,
+  View, 
+  StyleSheet, 
+  TouchableOpacity, 
+  Image 
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import 'react-native-gesture-handler';
+  
+const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 
-import {
-    GestureHandlerRootView,
-    PanGestureHandler,
-    PinchGestureHandler,
-    State,
-  } from 'react-native-gesture-handler';
-  
-  const baseScale = new Animated.Value(1);
-  const pinchScale = new Animated.Value(1);
-  const translateX = new Animated.Value(0);
-  const translateY = new Animated.Value(0);
-  
-  // ズームイベント
-  const onZoomEvent = Animated.event(
-    [
-      {
-        nativeEvent: { scale: pinchScale },
-      },
-    ],
-    {
-      useNativeDriver: true,
-    }
-  );
-  
-  // ドラッグイベント
-  const onPanEvent = Animated.event(
-    [
-      {
-        nativeEvent: {
-          translationX: translateX,
-          translationY: translateY,
-        },
-      },
-    ],
-    { useNativeDriver: true }
-  );
-  
-  const defaultPosition = { x: 0, y: 0, scale: 1 };
-  
-  export default function App({ navigation }: any ) {
-    const imagePan = React.createRef();
-    const imagePinch = React.createRef();
-    const [lastPosition, setLastPosition] = useState(defaultPosition);
-    const scale = Animated.multiply(baseScale, pinchScale);
+  export default function App({navigation}: any ) {
     return (
       <View style={styles.container}>
-        <GestureHandlerRootView>
-          <PanGestureHandler
-            ref={imagePan}
-            simultaneousHandlers={imagePinch}
-            onGestureEvent={onPanEvent}
-            onHandlerStateChange={(event) => {
-              if (event.nativeEvent.oldState === State.ACTIVE) {
-                // ドラッグ終了時点の値を次の開始地点として設定
-                const posX = lastPosition.x + event.nativeEvent.translationX;
-                const posY = lastPosition.y + event.nativeEvent.translationY;
-                setLastPosition({ x: posX, y: posY, scale: lastPosition.scale });
-                translateX.setOffset(posX);
-                translateX.setValue(0);
-                translateY.setOffset(posY);
-                translateY.setValue(0);
-              }
-            }}>
-            <Animated.View>
-              <PinchGestureHandler
-                ref={imagePinch}
-                simultaneousHandlers={imagePan}
-                onHandlerStateChange={(event) => {
-                  if (event.nativeEvent.oldState === State.ACTIVE) {
-                    // ズーム終了時点の値を次の開始スケール値として設定
-                    const lastScale =
-                    event.nativeEvent.scale < 1 ? 1 : event.nativeEvent.scale;
-                    setLastPosition({ ...lastPosition, scale: lastScale });
-                    baseScale.setValue(lastScale);
-                    pinchScale.setValue(1);
-                  }
-                }}
-                onGestureEvent={onZoomEvent}>
-                <TouchableOpacity onPress={() => navigation.navigate('Constellation_Creation')}>
-                    <Animated.Image
-                        source={require('../Assets/Frame 3.png')}
-                        resizeMode="contain"
+              <TouchableOpacity 
+                onPress={() => {navigation.navigate('Constellation_Creation')}}
+                activeOpacity={1}  
+              >
+                  <Image
+                        source={require('../Assets/Frame3.png')}
+                        resizeMode="cover"
                         style={[
                             {
-                            top: -300,
-                            left: -30,
                             width: 100,
                             height: 100,
+                            top: -100,
+                            left: -50,
                             },
-                            { transform: [{ scale }, { translateX }, { translateY }] },
                         ]}
-                    />
-                </TouchableOpacity>
-              </PinchGestureHandler>
-            </Animated.View>
-          </PanGestureHandler>
-        </GestureHandlerRootView>
+                  />
+              </TouchableOpacity>
       </View>
     );
-  }
+}
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 2,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: 'black',
-        padding: 500,
     },
     title: {
         fontSize: 20,
