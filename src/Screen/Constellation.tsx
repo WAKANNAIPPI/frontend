7a54d9ea649da24eb099b0312c249cad3e80f319
YuSzmiya <skyebell13reipgmail.com>
2022-09-01 10:58:07 +0900

    constellation.tsx内のデフォルト関数名をAppからConstellationに変更しました

--- src/Screen/Constellation.tsx
+++ src/Screen/Constellation.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
 
-export default function App() {
+export const Constellation: React.FC = () => {
     return (
         <View style={styles.inputContainer}>
             <TextInput style={styles.input} />
