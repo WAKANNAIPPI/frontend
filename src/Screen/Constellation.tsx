3eb507c1056d3e405aa4b3bb7624580850380cc6
Meri11Ra1 <R.t.00293794@gmail.com>
2022-09-04 11:14:59 +0900

    swipeのテスト

--- src/Screen/Constellation.tsx
+++ src/Screen/Constellation.tsx
@@ -1,39 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput, Animated } from 'react-native';
+import Swiper from 'react-native-swiper'
 
 export const Constellation: React.FC = () => {
     return (
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.input} />
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}></Text>
-                <Text>適当</Text>
-            </TouchableOpacity>
+        <View style={styles.page}>
+            <Swiper>
+                <View>
+                    <Text style={styles.testText}>
+                        Test1
+                    </Text>
+                </View>
+                <View>
+                    <Text style={styles.testText}>
+                        Test2
+                    </Text>
+                </View>
+                <View>
+                    <Text style={styles.testText}>
+                        Test3
+                    </Text>
+                </View>
+            </Swiper>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-        backgroundColor: '#222',
-    },
-    button: {
-        backgroundColor: 'rgb(29, 161, 242)',
-        paddingHorizontal: 20,
-        paddingVertical: 15,
-        borderRadius: 20,
-    },
-    buttonText: {
-        color: 'white',
-        fontweight: '900',
-        fontsize: 16,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-    },
     input: {
         flex: 1,
         borderColor: 'white',
@@ -41,10 +34,18 @@ const styles = StyleSheet.create({
         marginRight: 10,
         color: 'white',
         paddingHorizontal: 20,
-        fontsize: 20,
+        fontSize: 20,
     },
-    container: {
-        flex: 2,
-        paddingTop: 20,
+    slide: {
+        height: 500,
+        paddingHorizontal: 50,
+    },
+    page: {
+        flex: 1,
+        backgroundColor: '#232946'
+    },
+    testText: {
+        color: 'white',
+        fontSize: 30,
     }
 });
