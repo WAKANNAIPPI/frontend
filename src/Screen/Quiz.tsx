bf3ac07fb5ea92ce53794edcb9ec72df1e8f4f90
YuSzmiya <53136890+YuSzmiya@users.noreply.github.com>
2022-09-09 14:04:06 +0900

    Revert "スタイルシート作成"

--- src/Screen/Quiz.tsx
+++ src/Screen/Quiz.tsx
@@ -1,50 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
+import { Text } from "react-native";
 
 export const QuizScreen: React.FC = () => {
     return (
-    
-    <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>イーとする</Text>
-        </TouchableOpacity>
-    </View>
+
+    <Text>
+        これで大丈夫かな
+    </Text>
     
     )
+    
 }
-
-const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-        backgroundColor: '#222',
-    },
-    button: {
-        backgroundColor: 'rgb(29, 161, 242)',
-        paddingHorizontal: 20,
-        paddingVertical: 15,
-        borderRadius: 20,
-    },
-    buttonText: {
-        color: 'white',
-        fontweight: '900',
-        fontsize: 16,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-    },
-    input: {
-        flex: 1,
-        borderColor: 'white',
-        borderwidth: 2,
-        marginRight: 10,
-        color: 'white',
-        paddingHorizontal: 20,
-        fontsize: 20,
-    },
-    container: {
-        flex: 2,
-        paddingTop: 20,
-    }
-});
