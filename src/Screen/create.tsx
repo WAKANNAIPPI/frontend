926de8e167072b37696fc80ad53737246d8867a4
Meri11Ra1 <R.t.00293794@gmail.com>
2022-09-09 17:01:54 +0900

    保存用

--- src/Screen/create.tsx
+++ src/Screen/create.tsx
@@ -1,6 +1,45 @@
 import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 export const create: React.FC = () => {
-    // Constellationファイルにcreateコンポーネントへのリンクボタンを設置する
-    return (null)
+    return (
+        <>
+        <View style={styles.header}>
+            <View style={styles.completion}>
+                <Button
+                    title="完了"
+                    //onPress={() => } 完了（セーブ）
+                />
+            </View>
+        </View>
+        <View style={styles.pallet}>
+        </View>
+        <View style={styles.projection}>
+        </View>
+        </>
+    )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 0.1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#806BFF",
+    },
+    pallet: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+    projection: {
+        flex: 0.1,
+        backgroundColor: '#806BFF'
+    },
+    completion: {
+        Radius: 15,
+        width: 100,
+        height: 30,
+    }
+})
+
+
