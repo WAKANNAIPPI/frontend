2577a598eb9dc23d4e47d2debc429bc9301afbd3
chusehi <yu.nakamura.gamegod@gmail.com>
2022-10-02 10:29:16 +0900

    画面遷移できるようにしました

--- src/Screen/Quiz.tsx
+++ src/Screen/Quiz.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { QuizStackNavProp } from "../Navigations";
 import { StyleSheet, 
          Text, 
          View, 
@@ -6,9 +8,13 @@ import { StyleSheet,
          Image, 
          Button,
         } from 'react-native';
-        
+
+
 export const QuizScreen: React.FC = () => {
+    const navigation = useNavigation<QuizStackNavProp<'Quiz'>>()
+
     return (
+        <>
         <View style={styles.Container}>
             <Image style={{width:360, height:174}}
                 source = {require("../Assets/logo.png")}>
@@ -16,13 +22,15 @@ export const QuizScreen: React.FC = () => {
             <Image style={{width:277, height:56}}
                 source = {require("../Assets/Quiz/Quiz-start.png")}>
             </Image>
-            <View style={styles.Button}>
-                <Button
-                    title="ボタンをタップしてね"
-                    color="#806BFF"
-                />
-            </View>
         </View>
+        <View style={styles.Button}>
+            <Button
+                title="ボタンをタップしてね"
+                color="#806BFF"
+                onPress={() => navigation.navigate('Answer')}
+            />
+        </View>
+        </>
 
     )
 }
@@ -36,21 +44,10 @@ const styles = StyleSheet.create({
     Button:{
         flex: 1,
         alignItems: 'center',
-        paddingTop: 100,
-    },
-    buttonsContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
         justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        marginVertical: 20,
+        paddingTop: 100,
+        paddingBottom: 200,
+  
+        borderRadius: 100,
     },
-    subHeader: {
-        backgroundColor : "#2089dc",
-        color : "white",
-        textAlign : "center",
-        paddingVertical : 5,
-        marginBottom : 10
-    }
 });
