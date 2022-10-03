406a0257d18760e74c5ba8de14a48ade108bc53c
chusehi <yu.nakamura.gamegod@gmail.com>
2022-10-04 01:13:38 +0900

    ボタンを角丸にしてボタン配置終わりました

--- src/Screen/Answer.tsx
+++ src/Screen/Answer.tsx
@@ -8,21 +8,80 @@ import { StyleSheet,
          Image,
          Button,
         } from 'react-native';
-export const QuizScreen: React.FC = () => {
+export const Answer: React.FC = () => {
     const navigation = useNavigation<QuizStackNavProp<'Quiz'>>()
     return (
         <>
-        <View style={styles.}>
+        <View>
+            <Text
+              style={{
+                backgroundColor: "#BDBAFA",
+                height: 200,
+                fontSize: 30,
+            }}
+            >
+                ここに問題文を記述する予定です。
+            </Text>
         </View>
-        <View style={styles.}>
-            <Button
-                title="ボタンをタップしてね"
-                color="#806BFF"
-                onPress={() => navigation.navigate('')}
-            />
+        <View style={styles.container}>
+            <TouchableOpacity
+                style ={styles.Button}
+                onPress={() => navigation.navigate("")}
+            >
+                <Text style={styles.Textfont}>
+                    一つ目の選択肢
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style ={styles.Button}
+                onPress={() => navigation.navigate("")}
+            >
+                <Text style={styles.Textfont}>
+                    二つ目の選択肢
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style ={styles.Button}
+                onPress={() => navigation.navigate("")}
+            >
+                <Text style={styles.Textfont}>
+                    三つ目の選択肢
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style ={styles.Button}
+                onPress={() => navigation.navigate("")}
+            >
+                <Text style={styles.Textfont}>
+                    四つ目の選択肢
+                </Text>
+            </TouchableOpacity>
         </View>
         </>
     )
 }
 const styles = StyleSheet.create({
+    container:{
+        alignItems: 'center',
+        paddingBottom: 80,
+    
+    },
+    Answer:{
+        alignItems: 'center',
+        paddingTop: 300,
+    },
+    Button:{
+        backgroundColor: '#806BFF',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingRight: 60,
+        paddingLeft: 60,
+        paddingBottom: 10,
+        borderRadius: 100,
+        margin: 50,
+    },
+    Textfont:{
+        color: 'white',
+        fontWeight: 'normal',
+    },
 });
