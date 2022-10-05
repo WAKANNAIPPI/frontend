3402a7dbab2492d3f7adb44208a0cf1947fd353a
YuSzmiya <skyebell13rei@gmail.com>
2022-10-05 22:33:34 +0900

    円形配置

--- src/Screen/Home.tsx
+++ src/Screen/Home.tsx
@@ -1,15 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions } from 'react-native';
 import { RootStackNavProp } from "../Navigations";
 import ScatterChart from "react-native-scatter-chart";
 import { chartData } from "../Compoents/dot";
 
-export const HomeScreen: React.FC = () =>{
-    const navigation = useNavigation< RootStackNavProp<'Home'> >()
-    const {width, height} = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
+export const HomeScreen: React.FC = () => {
+    const navigation = useNavigation<RootStackNavProp<'Home'>>()
 
-    return(
+    return (
         <View style={styles.container}>
             <ScatterChart
                 backgroundColor='#282C3E'
@@ -19,62 +19,86 @@ export const HomeScreen: React.FC = () =>{
                 chartWidth={width}
             />
             <View style={styles.buttonLayout}>
-                <TouchableOpacity onPress={() => navigation.navigate('Constellation')} style={styles.button}>
-                    <Image
-                        source={require('../Assets/Frame1.png')}
-                        style={{ width: 50, height: 50 }}
-                    />
+                <View style={styles.constellation}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Constellation')} style={styles.button} >
+                        <Image
+                            source={require('../Assets/Frame1.png')}
+                            style={{ width: 50, height: 50 }}
+                        />
+                    </TouchableOpacity>
                     <Text style={{ color: 'white' }}>Constellation</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => navigation.navigate('Quiz')} style={styles.button}>
-                    <Image
-                        source={require('../Assets/Frame2.png')}
-                        style={{ width: 50, height: 50 }}
-                    />
+                </View>
+                <View style={styles.quiz}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Quiz')} style={styles.button} >
+                        <Image
+                            source={require('../Assets/Frame2.png')}
+                            style={{ width: 50, height: 50 }}
+                        />
+                    </TouchableOpacity>
                     <Text style={{ color: 'white' }}>Quiz</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => navigation.navigate('Projection')} style={styles.button}>
-                    <Image
-                        source={require('../Assets/Frame3.png')}
-                        style={{ width: 50, height: 50 }}
-                    />
+                </View>
+                <View style={styles.projection}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Projection')} style={styles.button}>
+                        <Image
+                            source={require('../Assets/Frame3.png')}
+                            style={{ width: 50, height: 50 }}
+                        />
+                    </TouchableOpacity>
                     <Text style={{ color: 'white' }}>Prpjection</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.button}>
-                    <Image
-                        source={require('../Assets/Frame4.png')}
-                        style={{ width: 50, height: 50 }}
-                    />
-                    <Text style={{ color: 'white' }>Login</Text>
-                </TouchableOpacity>
+                </View>
+                <View style={styles.login}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.button}>
+                        <Image
+                            source={require('../Assets/Frame4.png')}
+                            style={{ width: 50, height: 50 }}
+                        />
+                    </TouchableOpacity>
+                    <Text style={{ color: 'white' }}>Login</Text>
+                </View>
             </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
         position: 'relative',
 
     },
-    chart:{
+    chart: {
         position: 'absolute',
-        flex:1,
     },
-    buttonLayout:{
-        position:'absolute',
-        flexDirection: 'column',
-        alignSelf:'center',
-        marginTop: 80,
-        marginHorizontal: 16,
+    buttonLayout: {
+        position: 'absolute',
+        alignSelf: 'center',
+        marginTop: Dimensions.get('window').width ,
+        Radius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+        width: Dimensions.get('window').width * 0.5,
+        height: Dimensions.get('window').width * 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    button:{
-        flex:1,
-
+    button: {
+        height: 50,
+        width: 50,
+    },
+    constellation: {
+        position: "absolute",
+        bottom: Dimensions.get('window').width * 0.35
+    },
+    quiz: {
+        position: "absolute",
+        top: Dimensions.get('window').width * 0.2,
+        right: Dimensions.get('window').width * 0.4
+    },
+    projection: {
+        position: "absolute",
+        top: Dimensions.get('window').width * 0.4
+    },
+    login: {
+        position: "absolute",
+        left: Dimensions.get('window').width * 0.4
     },
 })
 
